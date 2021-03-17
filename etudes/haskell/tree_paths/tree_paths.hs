2978fb3890232209e922b0ba39646463020a974b
Sergei Blinov <blinovsv@gmail.com>
2021-03-17 20:33:08 +0300

    update

--- etudes/haskell/tree_paths/tree_paths.hs
+++ etudes/haskell/tree_paths/tree_paths.hs
@@ -1,19 +1,17 @@
 -- https://www.codingame.com/ide/puzzle/tree-paths
 import Data.Map
 
-main = interact $ unwords.f.fmap read.words
-
-makeMap [] m = m
-makeMap (p:l:r:rest) m = insert r (p, "Right").insert l (p, "Left").makeMap rest $ m
+main = interact $ unwords . f . fmap read . tail . words
 
 f :: [Integer] -> [[Char]]
-f (_:v:_:es)
-    | p == [] = ["Root"]
-    | otherwise = p
-    where
-        m = makeMap es empty
-        p = path v m
+f (v : _ : edges) = ans $ path v dict
+  where
+    extract [] = []
+    extract (p : l : r : es) = (l, (p, "Left")) : (r, (p, "Right")) : extract es
+    dict = fromList $ extract edges
+    ans [] = ["Root"]
+    ans x = x
 
 path v m
-    | notMember v m = []
-    | otherwise = let (vv, dir) = m ! v in path vv m ++ [dir]
+  | notMember v m = []
+  | otherwise = let (vv, dir) = m ! v in path vv m ++ [dir]
